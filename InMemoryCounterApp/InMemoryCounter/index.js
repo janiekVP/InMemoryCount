3d1fed7c7d5d635ffe15fc57f2c31f72a3e585a2
Janiek van Pelt <36726853+janiekVP@users.noreply.github.com>
2025-06-06 11:51:57 +0200

    Delete InMemoryCounterApp directory

--- InMemoryCounterApp/InMemoryCounter/index.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-module.exports = async function (context, req) {
-    count++;
-    context.res = {
-        status: 200,
-        body: `This function has been called ${count} times.`
-    };
-};
